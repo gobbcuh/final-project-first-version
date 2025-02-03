1dbf8e5d72a9716f4c3b9577b98aeaa7c275f7e3
gobbcuh <jajalorino@gmail.com>
2025-02-03 20:26:32 +0800

    first commit added game intro draft w loading bar

---
+++ src/learningJava/com/Constants.java
@@ -0,0 +1,10 @@
+package learningJava.com;
+
+public class Constants
+{
+    public static void main(String[] args)
+    {
+        final String COMPANY_NAME = "Jasmin Company";
+        System.out.println(COMPANY_NAME);
+    }
+}
