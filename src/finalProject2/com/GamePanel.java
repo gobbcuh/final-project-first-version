54162e4d7749e4653e7b8ab2e366200509bd1e4b
gobbcuh <jajalorino@gmail.com>
2025-02-27 18:25:27 +0800

    walk keys

--- src/finalProject2/com/GamePanel.java
+++ src/finalProject2/com/GamePanel.java
@@ -15,12 +15,20 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -39,14 +47,25 @@ public class GamePanel extends JPanel implements Runnable{
         }
     }
     public void update() {
-
+        if (keyH.upPressed == true) {
+            playerY = playerY - playerSpeed;
+        }
+        else if (keyH.downPressed == true) {
+            playerY = playerY + playerSpeed;
+        }
+        else if (keyH.leftPressed == true) {
+            playerX = playerX - playerSpeed;
+        }
+        else if (keyH.rightPressed == true) {
+            playerX = playerX + playerSpeed;
+        }
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
 
         g2.setColor(Color.white);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         g2.dispose();
     }
 }
