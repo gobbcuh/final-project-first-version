b2b5ff0faf6bc0cbda788d894c44d0208b35b25d
gobbcuh <jajalorino@gmail.com>
2025-02-27 18:52:38 +0800

    tried another walk keys function method

--- src/finalProject2/com/GamePanel.java
+++ src/finalProject2/com/GamePanel.java
@@ -40,28 +40,46 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     @Override
+//    public void run() {
+//        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+//        double nextDrawTime = System.nanoTime() + drawInterval;
+//
+//        while (gameThread != null) {
+//            // 1. UPDATE: Update information such as character positions
+//            update();
+//            // 2. DRAW: Draw the screen with the updated information
+//            repaint();
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime = remainingTime / 1000000;
+//
+//                if (remainingTime < 0) {
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long)remainingTime);
+//                nextDrawTime += drawInterval;
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//       }
+//  }
     public void run() {
-        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
-        double nextDrawTime = System.nanoTime() + drawInterval;
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
 
         while (gameThread != null) {
-            // 1. UPDATE: Update information such as character positions
-            update();
-            // 2. DRAW: Draw the screen with the updated information
-            repaint();
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
 
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime / 1000000;
-
-                if (remainingTime < 0) {
-                    remainingTime = 0;
-                }
-
-                Thread.sleep((long)remainingTime);
-                nextDrawTime += drawInterval;
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
             }
         }
     }
