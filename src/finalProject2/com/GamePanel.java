91db8bc9aebfe7b99d560a1b94b8ec6fcd588f42
gobbcuh <jajalorino@gmail.com>
2025-02-27 17:38:25 +0800

    added game panel class

--- src/finalProject2/com/GamePanel.java
+++ src/finalProject2/com/GamePanel.java
@@ -1,4 +1,23 @@
 package finalProject2.com;
 
-public class GamePanel {
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class GamePanel extends JPanel {
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16 x 16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48 x 48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
 }
