5df43b6cd21809a29ad1e84e09140d94b7bd99bd
gobbcuh <jajalorino@gmail.com>
2025-02-28 23:20:09 +0800

    added ui text key

--- src/entity/com/Player.java
+++ src/entity/com/Player.java
@@ -13,7 +13,7 @@ public class Player extends Entity {
     KeyHandler keyH;
     public final int screenX;
     public final int screenY;
-    int hasKey = 0;
+    public int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
