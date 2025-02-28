f2acff037130e199095e21e44bffa9dbf1263b76
gobbcuh <jajalorino@gmail.com>
2025-02-28 13:26:07 +0800

    entity added

---
+++ src/finalProject2/com/CollisionChecker.java
@@ -0,0 +1,13 @@
+package finalProject2.com;
+
+import entity.com.Entity;
+
+public class CollisionChecker {
+    GamePanel gp;
+    public CollisionChecker(GamePanel gp) {
+        this.gp = gp;
+    }
+    public void checkTile(Entity entity) {
+
+    }
+}
