144f2731511c36b6903b7e76958fdcdf7ed8895c
gobbcuh <jajalorino@gmail.com>
2025-02-28 11:21:44 +0800

    updated tiles (new method)

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -30,6 +30,24 @@ public class TileManager {
         }
     }
     public void draw(Graphics2D g2) {
+        int col = 0;
+        int row = 0;
+        int x = 0;
+        int y = 0;
+
+        while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+            g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
+            col++;
+            x += gp.tileSize;
+
+            if (col == gp.maxScreenCol) {
+                col = 0;
+                x = 0;
+                row++;
+                y += gp.tileSize;
+            }
+        }
+
 //        g2.drawImage(tile[1].image, 0, 0, gp.tileSize, gp.tileSize, null);
 //        g2.drawImage(tile[1].image, 48, 0, gp.tileSize, gp.tileSize, null);
 //        g2.drawImage(tile[1].image, 96, 0, gp.tileSize, gp.tileSize, null);
